5563a0f784efef8fd03f2c78fe297f1fc9873857
Grace-Endy <graceendylaine@yahoo.fr>
2018-09-28 20:33:42 -0400

    Initial commit 3
    Signed-off-by: Grace-Endy <graceendylaine@yahoo.fr>

---
+++ app/src/main/java/com/goopam/simpletodo/Splashscreen.java
@@ -0,0 +1,78 @@
+package com.goopam.simpletodo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import static java.lang.Thread.sleep;
+
+public class Splashscreen extends Activity {
+
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+    }
+
+    /**
+     * Call when the activity is first created
+     */
+    Thread splashThread;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        StartAnimatiom();
+    }
+
+    private void StartAnimatiom() {
+
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.reset();
+        LinearLayout l = (LinearLayout) findViewById(R.id.lin_lay);
+        l.clearAnimation();
+        l.startAnimation(anim);
+
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+
+        ImageView iv = (ImageView) findViewById(R.id.splash);
+        iv.clearAnimation();
+        iv.startAnimation(anim);
+
+        splashThread = new Thread(){
+
+            @Override
+
+            public void run () {
+                try {
+                    int waited = 0;
+                    // Splash Screen Pause Time
+                    while (waited < 5000) {
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(Splashscreen.this, MainActivity.class);
+                    intent.setFlags(intent.FLAG_ACTIVITY_NO_ANIMATION);
+
+                    startActivity(intent);
+                    Splashscreen.this.finish();
+                } catch (InterruptedException e) {
+                    //Do nothing
+                } finally {
+                    Splashscreen.this.finish();
+                }
+            }
+        };
+        splashThread.start();
+
+    }
+}
+
